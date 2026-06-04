706f756a011881fd2e7c25e0c84fd08d08614867
Alexandra <64429585+mwalexandra@users.noreply.github.com>
2026-06-04 14:05:56 +0000

    widget works well

--- widget-angular/src/app/app.ts
+++ widget-angular/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ConfiguratorWidgetComponent } from './configurator-widget/configurator-widget.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ConfiguratorWidgetComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('widget-angular');
-}
+export class App {}
