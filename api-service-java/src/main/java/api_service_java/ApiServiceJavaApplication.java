25a781da982e22d132583be9ec06a13b04dc4f4a
Alexandra <64429585+mwalexandra@users.noreply.github.com>
2026-06-04 12:04:19 +0000

    Spring Mavem init

---
+++ api-service-java/src/main/java/api_service_java/ApiServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package api_service_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceJavaApplication.class, args);
+	}
+
+}
---
+++ api-service-java/src/test/java/api_service_java/ApiServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package api_service_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
