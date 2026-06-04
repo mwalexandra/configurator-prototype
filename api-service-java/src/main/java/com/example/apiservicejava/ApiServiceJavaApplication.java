cff84643de423059ea61ec287bd18818defc8158
Alexandra <64429585+mwalexandra@users.noreply.github.com>
2026-06-04 12:40:14 +0000

    health controller fixed and works

--- api-service-java/src/main/java/com/example/apiservicejava/ApiServiceJavaApplication.java
+++ api-service-java/src/main/java/com/example/apiservicejava/ApiServiceJavaApplication.java
@@ -1,4 +1,4 @@
-package api_service_java;
+package com.example.apiservicejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
