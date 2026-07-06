db410d9426aef14e0b81d4ca0ef804e3b1d7f2a1
Alexandra Makeewa <alexandra.makeewa@bauerfeind.com>
2026-07-06 07:35:27 +0000

    api-service url changed

--- widget-angular/src/environments/environment.ts
+++ widget-angular/src/environments/environment.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: false,
   apiUrl: '/api'
 }; 
-// https://port8080-workspaces-ws-qkpkj.us10.trial.applicationstudio.cloud.sap
+// https://port8080-workspaces-ws-hou1r.us10.trial.applicationstudio.cloud.sap/
