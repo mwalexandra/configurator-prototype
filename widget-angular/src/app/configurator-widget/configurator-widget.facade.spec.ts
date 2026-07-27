01020d7ee71c146f5b59467e1a99f62f95b12ced
Alexandra Makeewa <alexandra.makeewa@bauerfeind.com>
2026-07-27 08:15:08 +0000

    facade placeholder-tests

---
+++ widget-angular/src/app/configurator-widget/configurator-widget.facade.spec.ts
@@ -0,0 +1,7 @@
+import { ConfiguratorWidgetFacade } from './configurator-widget.facade';
+
+describe('ConfiguratorWidgetFacade', () => {
+  it('placeholder', () => {
+    expect(true).toBe(true);
+  });
+});
